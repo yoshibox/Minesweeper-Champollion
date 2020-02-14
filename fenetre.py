157f977319ea70192a7ef043d8217dc568e74f6a
BuildTools <unconfigured@null.spigotmc.org>
2020-02-14 09:56:51 +0100

    board draw

--- fenetre.py
+++ fenetre.py
@@ -2,14 +2,24 @@ from tkinter import *
 
 class Board: 
     def __init__(self, n): # n = côté du tableau
-        self.WIDTH = 1280
-        self.HEIGHT = 720
-        self.buttonSizeL = self.WIDTH / n  # Largeur des boutons
-        self.buttonSizeH = self.HEIGHT / n # Hauteur des boutons
-        Tk()
-
+        self.WIDTH = 800
+        self.HEIGHT = 600
+        self.wh = n   # Nombre de boutons sur la largeur et la hauteur
+        self.bSizeL = self.WIDTH / self.wh  # Largeur des boutons
+        self.bSizeH = self.HEIGHT / self.wh # Hauteur des boutons
+        self.board = []
+        self.window = Tk()
+        self.canvas = Canvas(self.window, width=self.WIDTH, height=self.HEIGHT, background="gray")
+        self.canvas.pack()
+        self.__drawBoard__()
+    
+    def __drawBoard__(self):
+        for i in range(self.wh):
+            for j in range(self.wh):
+                self.canvas.create_rectangle(i * self.bSizeL,  j * self.bSizeH, i * self.bSizeL + self.bSizeL, j * self.bSizeH + self.bSizeH, fill="pink")
 
 
 
 if __name__ == "__main__":
-    Board(42)
+    tableau = Board(12)
+    tableau.window.mainloop()

dad3c42c55cb7410c0779728b16809393aec996e
404-42 <46428153+404-42@users.noreply.github.com>
2020-02-29 18:31:29 +0100

    Add files via upload

--- fenetre.py
+++ fenetre.py
@@ -1,6 +1,7 @@
 from tkinter import *
+from logique import *
 
-class Board: 
+class Board:
     def __init__(self, n): # n = côté du tableau
         self.WIDTH = 800
         self.HEIGHT = 600
@@ -12,14 +13,17 @@ class Board:
         self.canvas = Canvas(self.window, width=self.WIDTH, height=self.HEIGHT, background="gray")
         self.canvas.pack()
         self.__drawBoard__()
-    
+
     def __drawBoard__(self):
         for i in range(self.wh):
             for j in range(self.wh):
                 self.canvas.create_rectangle(i * self.bSizeL,  j * self.bSizeH, i * self.bSizeL + self.bSizeL, j * self.bSizeH + self.bSizeH, fill="pink")
 
-
-
 if __name__ == "__main__":
-    tableau = Board(12)
+    n = 12
+    tableau = Board(n)
+    logic = logique(n, tableau)
+
+    tableau.canvas.bind("<Button-1>", logic.choix_user)
+
     tableau.window.mainloop()
---
+++ logique.py
@@ -0,0 +1,58 @@
+"""
+@author: BOTTI Joakim
+"""
+
+from random import sample, randrange
+from os import system
+import sys
+from time import sleep
+
+"""
+dans la matrice:
+    bombe: representer par le nombre 42
+    autre: nombre de bombe a proximiter
+information du plateau:
+    width: 800px
+    height: 600px
+"""
+
+
+class logique:
+    nb_bombe = 24
+    show_case = []
+
+    def __init__(self, n, tableau):
+        self.n = n
+        self.tableau = tableau
+        self.state = self.random_platform(self.n, self.nb_bombe)
+
+    def affiche(self):
+        print('\n'.join(' '.join(map(str, L)) for L in self.state))
+
+    def voisins(self, n, i, j):
+        return [(a,b) for (a, b) in [(i, j+1),(i, j-1),(i-1, j),(i+1,j),(i+1,j+1),(i-1,j-1),(i-1,j+1),(i+1,j-1)] if a in range(n) and b in range(n)]
+
+    def random_platform(self, n, nb_bombe):
+        units=[(line,col) for col in range(n) for line in range(n)]
+        bombe=sample(units, nb_bombe)
+        state=[[0]*n for _ in range(n)]
+        for (i,j) in bombe:
+            state[i][j]=42
+            voisin = self.voisins(n, i, j)
+            for x, y in voisin:
+                if state[x][y] != 42: state[x][y] += 1
+        return state
+
+    def choix_user(self, e):
+        x, y = e.x//67, e.y//50
+        self.show_case.append(self.tableau.canvas.create_text(
+            x*(800//12)+(800//24),
+            y*(600/12)+(600//24),
+            text=self.state[x][y]
+        ))
+
+
+
+if __name__ == "__main__":
+    logic = logique(0xc)
+    logic.affiche()

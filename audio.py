e4d8f133ddb4f9f30d45413d1629cba8460f57f6
yoshibox <40672404+yoshibox@users.noreply.github.com>
2020-05-10 16:47:16 +0200

    Game can still work if pygame is not installed

--- audio.py
+++ audio.py
@@ -1,22 +1,33 @@
-from pygame import mixer
+pygameInstalled = True
 
+try:
+    from pygame import mixer
+except ModuleNotFoundError:
+    print("Il semble que pygame n'est pas installé, vous pouvez l'installer avec \"pip install --user pygame\"\n\
+Vous pouvez continuer à jouer, mais vous n'aurez pas de son")
+    pygameInstalled = False
 
 class audio:
 
     def __init__(self, default):
+        if not pygameInstalled: return
         mixer.init()
         mixer.music.load(default)
 
     def stop(self):
+        if not pygameInstalled: return
         mixer.music.stop()
 
     def play(self):
+        if not pygameInstalled: return
         mixer.music.play()
 
     def game_over(self):
+        if not pygameInstalled: return
         mixer.music.load("assets/lost.mp3")
         mixer.music.play()
 
     def win(self):
+        if not pygameInstalled: return
         mixer.music.load("assets/win.mp3")
         mixer.music.play()
